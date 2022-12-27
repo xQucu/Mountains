dda3352efca371d7d07f7ac3b83d383a34d678e5
Adrian Kucyk <kucykadrian2004@gmail.com>
2022-12-27 22:11:25 +0100

    repaired navbar

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,33 @@
 $(function () {
+  var scrollTop = $(this).scrollTop();
+  if (scrollTop > 300) {
+    $('.visibility-toggler-box').show();
+    $('.box-hook').removeClass('box');
+    $('.orbit-hook').removeClass('orbit');
+    $('.orbit1-hook').removeClass('orbit1');
+    $('.orbit2-hook').removeClass('orbit2');
+    $('.orbit3-hook').removeClass('orbit3');
+    $('.orbit4-hook').removeClass('orbit4');
+    $('.orbit5-hook').removeClass('orbit5');
+    $('.orbit6-hook').removeClass('orbit6');
+    $('.orbit7-hook').removeClass('orbit7');
+    $('.orbit8-hook').removeClass('orbit8');
+    $('.in-center-hook').removeClass('in-center box');
+  } else {
+    $('.visibility-toggler-box').off();
+    $('.visibility-toggler-box').hide();
+    $('.box-hook').addClass('box');
+    $('.orbit-hook').addClass('orbit');
+    $('.orbit1-hook').addClass('orbit1');
+    $('.orbit2-hook').addClass('orbit2');
+    $('.orbit3-hook').addClass('orbit3');
+    $('.orbit4-hook').addClass('orbit4');
+    $('.orbit5-hook').addClass('orbit5');
+    $('.orbit6-hook').addClass('orbit6');
+    $('.orbit7-hook').addClass('orbit7');
+    $('.orbit8-hook').addClass('orbit8');
+    $('.in-center-hook').addClass('in-center box');
+  }
   $('.visibility-toggler-box').html(`<input
             type="checkbox"
             class="btn-check"
